ba450a970c3d1809fe5e3843cfc197fcd1e4fc02
Jonatas Santos <jonatas.santos@optiisolutions.com>
2025-10-08 11:26:52 -0300

    Update project metadata, add new blog post structure, and enhance TypeScript configuration. Rename project in package files, add new dependencies for markdown processing, and implement API functions for blog posts.

---
+++ app/posts/[slug]/page.tsx
@@ -0,0 +1,54 @@
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { getAllPosts, getPostBySlug } from "@/src/lib/api";
+import markdownToHtml from "@/src/lib/markdownToHtml";
+
+export default async function Post(props: Params) {
+  const params = await props.params;
+  const post = getPostBySlug(params.slug);
+
+  if (!post) {
+    return notFound();
+  }
+
+  const content = await markdownToHtml(post.content || "");
+
+  return (
+    <main>
+     {content}
+    </main>
+  );
+}
+
+type Params = {
+  params: Promise<{
+    slug: string;
+  }>;
+};
+
+export async function generateMetadata(props: Params): Promise<Metadata> {
+  const params = await props.params;
+  const post = getPostBySlug(params.slug);
+
+  if (!post) {
+    return notFound();
+  }
+
+  const title = `${post.title} | Next.js Blog Example with`;
+
+  return {
+    title,
+    openGraph: {
+      title,
+      images: [post.ogImage.url],
+    },
+  };
+}
+
+export async function generateStaticParams() {
+  const posts = getAllPosts();
+
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+}

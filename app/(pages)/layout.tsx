7d10748b3e4f7fce3251681379831d2381b702e4
Jonatas Santos <jonatas.santos@optiisolutions.com>
2025-10-14 17:21:22 -0300

    Enhance layout metadata for improved SEO and social sharing
    - Added comprehensive metadata including keywords, authors, and open graph details to enhance search engine optimization and social media visibility.
    - Updated the site URL and included Twitter card information for better integration with social platforms.
    - Configured robots meta tags to optimize indexing and crawling behavior.

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -3,12 +3,60 @@ import { Inter } from 'next/font/google'
 import '@/styles/globals.css'
 import Template from 'app/components/template'
 import favicon from '@/imgs/jonatas-ricardo-santos-frontend-icon.png'
+import ogImage from '@/imgs/jonatas-ricardo-santos-frontend-icon.png'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Jonatas Santos - Fullstack Software Engineer',
+  title: 'Jonatas Ricardo Santos - Fullstack Software Engineer',
   description: 'Senior Fullstack Engineer specializing in React, Next.js, and AI/ML. Building scalable products and sharing insights on modern web engineering.',
+  keywords: ['Jonatas Ricardo Santos', 'Fullstack Engineer','Frontend Engineer', 'React', 'typescript', 'Next.js', 'AI/ML', 'Software Engineer', 'Portfolio'],
+  authors: [{ name: 'Jonatas Ricardo Santos' }],
+  creator: 'Jonatas Ricardo Santos',
+  publisher: 'Jonatas Ricardo Santos',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  metadataBase: new URL('https://jonatasricardo.com'),
+  alternates: {
+    canonical: '/',
+  },
+  openGraph: {
+    title: 'Jonatas Ricardo Santos - Fullstack Software Engineer',
+    description: 'Senior Fullstack Engineer specializing in React, Next.js, and AI/ML. Building scalable products and sharing insights on modern web engineering.',
+    url: 'https://jonatasricardo.com',
+    siteName: 'Jonatas Ricardo Santos Portfolio',
+    images: [
+      {
+        url: ogImage.src,
+        width: 1200,
+        height: 630,
+        alt: 'Jonatas Ricardo Santos - Fullstack Software Engineer',
+      },
+    ],
+    locale: 'pt_BR',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Jonatas Ricardo Santos - Fullstack Software Engineer',
+    description: 'Senior Fullstack Engineer specializing in React, Next.js, and AI/ML. Building scalable products and sharing insights on modern web engineering.',
+    images: [ogImage.src],
+    creator: '@JonatasRicardo',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
   icons: {
     icon: favicon.src,
     shortcut: favicon.src,

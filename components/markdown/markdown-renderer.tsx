cfc72875171760887e0e0759dab7e48f94c50a49
JonatasRicardo <jonatasricardo90@gmail.com>
2026-04-28 16:56:38 -0300

    feat(markdown):  Markdown undo

--- components/markdown/markdown-renderer.tsx
+++ components/markdown/markdown-renderer.tsx
@@ -2,32 +2,36 @@
 
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
+import { gruvboxDark } from "react-syntax-highlighter/dist/esm/styles/prism"
 import remarkGfm from 'remark-gfm'
-import { cn } from '../base-ui/utils'
-import markdownStyles from "./markdown-styles.module.css";
-
 interface MarkdownRendererProps {
   content: string,
   className?: string
 }
 
 interface CodeComponentProps {
+  node?: any
   inline?: boolean
   className?: string
   children?: React.ReactNode
+  [key: string]: any
 }
 
 function CodeRender({ inline, className, children, ...props }: CodeComponentProps) {
-  return inline ? (
+  const match = /language-(\w+)/.exec(className || '')
+  const language = match ? match[1] : '';
+  return !inline && match ? (<SyntaxHighlighter
+      style={gruvboxDark}
+      language={language}
+      PreTag="div"
+      className="rounded-md !mb-6"
+    >
+      {String(children).replace(/\n$/, '')}
+    </SyntaxHighlighter> ) : (
     <code className={className} {...props}>
       {children}
     </code>
-  ) : (
-    <pre className="mb-6 overflow-x-auto rounded-md bg-indigo-950 p-4 text-gray-100">
-      <code className={className} {...props}>
-        {String(children).replace(/\n$/, '')}
-      </code>
-    </pre>
   )
 }
 
@@ -39,18 +43,22 @@ function LinkRenderer({ href, children, ...props }: React.AnchorHTMLAttributes<H
   );
 }
 
-export function MarkdownRenderer({ content, className }: MarkdownRendererProps) {
+
+export function MarkdownRenderer({ content }: MarkdownRendererProps) {
   return (
-    <div className={cn("prose prose-sm dark:prose-invert max-w-none prose-pre:bg-indigo-950 prose-pre:text-gray-100", markdownStyles["markdown"], className)}>
+    <div className="prose prose-sm dark:prose-invert max-w-none prose-pre:bg-indigo-950 prose-pre:text-gray-100">
       <ReactMarkdown
         remarkPlugins={[remarkGfm]}
         components={{
           code: CodeRender,
           a: LinkRenderer,
+          p: ({ children }) => <p className="mt-6 mb-6">{children}</p>,
+          h2: ({ children }) => <h2 className="mt-8 mb-8">{children}</h2>,
+          img: ({ children, ...props }) => <span className='flex justify-center'><img {...props} className="mt-4 mb-4 lg:max-w-[700px]">{children}</img></span>
         }}
       >
         {content}
       </ReactMarkdown>
     </div>
   )
-}
+} 
